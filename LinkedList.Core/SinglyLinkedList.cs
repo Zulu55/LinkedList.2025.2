350bcfa730893893994888c54370efa988f6fdc6
Juan Zuluaga <jzuluaga55@gmail.com>
2025-10-02 21:12:33 -0500

    Basic example for single list

---
+++ LinkedList.Core/SinglyLinkedList.cs
@@ -0,0 +1,47 @@
+﻿namespace LinkedList.Core;
+
+public class SinglyLinkedList<T>
+{
+    private SimpleNode<T>? _head;
+
+    public SinglyLinkedList()
+    {
+        _head = null;
+    }
+
+    public void InsertAtBeginning(T data)
+    {
+        var newNode = new SimpleNode<T>(data);
+        newNode.Next = _head;
+        _head = newNode;
+    }
+
+    public void InsertAtEnd(T data)
+    {
+        var newNode = new SimpleNode<T>(data);
+        if (_head == null)
+        {
+            _head = newNode;
+        }
+        else
+        {
+            var current = _head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+
+    public void PrintList()
+    {
+        var current = _head;
+        while (current != null)
+        {
+            Console.Write($"{current.Data} -> ");
+            current = current.Next;
+        }
+        Console.WriteLine("null");
+    }
+}
